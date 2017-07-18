6255e71443e5224af15241d1f5d67ca5b924051e
jmedran <jmedran@gmail.com>
2017-07-17 18:54:20 -0700

    before changing blink scroll

--- app/app.js
+++ app/app.js
@@ -1,83 +1,26 @@
 import React, {Component} from 'react';
-import {App, Sphere, Tetrahedron, Box} from '../src/index';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Grid, Row, Col} from 'react-flexbox-grid';
-import {Footer, Jumbo} from './components/layout';
-import {MeshBasicMaterial} from 'three';
-
-import {SceneModule, CameraModule, RenderingModule} from 'whs/src/modules/app/export';
-import {OrbitModule} from 'whs/src/modules/controls/export';
-
-import {BasicSphere} from './components/BasicSphere';
+import {Footer, Jumbo, Background} from './components/layout';
 
 export class Application extends Component {
-
-  constructor(props) {
-    super(props);
-    const spheres = new Array(100).fill(0);
-
-    this.state = {
-      spheres: spheres,
-      opacity: 0.5,
-      position: Math.random() * 800 - 400,
-      color: Math.random() * 0xffffff,
-      geometry: [3, 35, 35],
-      camera: {
-        position: {
-          z: 20
-        }
-      }
-    }
-  }
-
   render() {
-    this.state.spheres.fill(20);
     return (
-      <Grid fluid>
-        <Row>
-          <Col md={12}>
-            <Jumbo />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-          </Col>
-        </Row>
-        <Footer />
-        <App modules={[
-          new SceneModule(),
-          new CameraModule({
-            position: {
-              z: this.state.camera.position.z
-            }
-          }),
-          new RenderingModule({
-            bgOpacity: 0,
-            renderer: {alpha: true}
-          }),
-          new OrbitModule()
-        ]}
-             refApp={app => {
-               console.log(app); // app
-             }}
-        >
-          {
-            this.state.spheres.map(function (result, id) {
-              return (
-                <Sphere
-                  geometry={this.state.geometry}
-                  material={new MeshBasicMaterial({color: Math.random() * 0xffffff, opacity: this.state.opacity})}
-                  key={id}
-                  refComponent={component => {
-                    console.log(component); // component
-                  }}
-                  position={[Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400]}
-                />
-              )
-            }, this)
-          }
-        </App>
-      </Grid>
+      <div>
+        <Background/>
+        <Grid fluid>
+          <Row>
+            <Col md={12}>
+              <Jumbo />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+            </Col>
+          </Row>
+          <Footer />
+        </Grid>
+      </div>
     )
   }
 }
---
+++ app/components/layout/Background.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {App, Sphere} from '../../../src/index';
+import {MeshBasicMaterial} from 'three';
+import {SceneModule, CameraModule, RenderingModule} from 'whs/src/modules/app/export';
+import {OrbitModule} from 'whs/src/modules/controls/export';
+
+export class Background extends React.Component {
+  constructor(props) {
+    super(props);
+    const spheres = new Array(100).fill(0);
+
+    this.state = {
+      spheres: spheres,
+      opacity: 0.5,
+      position: Math.random() * 800 - 400,
+      color: Math.random() * 0xffffff,
+      geometry: [3, 35, 35],
+      camera: {
+        position: {
+          z: 20
+        }
+      }
+    }
+  }
+  render() {
+    return (
+      <App modules={[
+        new SceneModule(),
+        new CameraModule({
+          position: {
+            z: this.state.camera.position.z
+          }
+        }),
+        new RenderingModule({
+          bgOpacity: 0,
+          renderer: {alpha: true}
+        }),
+        new OrbitModule({
+          controls: {
+            noPan: true
+          }
+
+        })
+      ]}
+           // refApp={app => {
+           //   console.log(app); // app
+           // }}
+      >
+        {
+          this.state.spheres.map(function (result, id) {
+            return (
+              <Sphere
+                geometry={this.state.geometry}
+                material={new MeshBasicMaterial({color: Math.random() * 0xffffff, opacity: this.state.opacity})}
+                key={id}
+                // refComponent={component => {
+                //   console.log(component); // component
+                // }}
+                position={[Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400]}
+              />
+            )
+          }, this)
+        }
+      </App>
+    )
+  }
+}
